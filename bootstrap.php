7e6fc2990a2485c1363ca8670e5ed9e1c5a7c96d
Harold <mail@yaolong.me>
2016-06-12 16:50:57 +0800

    init

--- bootstrap.php
+++ bootstrap.php
@@ -1,20 +1,23 @@
 <?php
+define('IMPRESS_START', microtime(true));
+
 define("ROOT", dirname(__FILE__));
 define("CONFIG_DIR", ROOT . DIRECTORY_SEPARATOR . "config");
 define("APP_DIR", ROOT . DIRECTORY_SEPARATOR . "app" . DIRECTORY_SEPARATOR . "App");
 define("CRON_DIR", ROOT . DIRECTORY_SEPARATOR . "app" . DIRECTORY_SEPARATOR . "Cron");
 define("HTTP_DIR", ROOT . DIRECTORY_SEPARATOR . "app" . DIRECTORY_SEPARATOR . "Http");
-define("LOG_DIR", ROOT . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "log");
-define("CACHE_DIR", ROOT . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "cache");
-define("DEBUG", 0);
-
-if (DEBUG === 1) {
-    ini_set("display_errors", 1);
-    error_reporting(E_ALL);
-}
+define("DATA_DIR", ROOT . DIRECTORY_SEPARATOR . "data");
+define("LOG_DIR", DATA_DIR . DIRECTORY_SEPARATOR . "log");
+define("PID_DIR", DATA_DIR . DIRECTORY_SEPARATOR . "pid");
+define("CACHE_DIR", DATA_DIR . DIRECTORY_SEPARATOR . "cache");
 
 require_once ROOT . DIRECTORY_SEPARATOR . "vendor/autoload.php";
 
 // require config|env files
 $dotenv = new \Dotenv\Dotenv(CONFIG_DIR);
 $dotenv->load();
+
+if (boolval(getenv("DEBUG_DISPLAY_ERRORS"))) {
+    ini_set("display_errors", 1);
+    error_reporting(E_ALL);
+}

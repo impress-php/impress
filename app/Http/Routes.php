23b43806a638a9203ba2b0b662c08c159cc7b0f8
Harold <mail@yaolong.me>
2016-06-17 17:08:43 +0800

    add Routes

--- app/Http/Routes.php
+++ app/Http/Routes.php
@@ -7,15 +7,27 @@ Route::post("/", function () {
 Route::get("/", "HelloWorld@index");
 
 Route::group([
-    "prefix" => "/s"
+    "prefix" => "/s",
+    "middleware" => [
+        "a", "b", "c"
+    ]
 ], function () {
-    Route::post("/", function () {
-        echo "xxxxxxxx";
+    Route::group([
+        "prefix" => "/f",
+        "middleware" => [
+            "d", "e", "f"
+        ]
+    ], function () {
+        Route::post("/a", function () {
+            echo "xxxxxxxx";
+        }, ["xxxx"]);
+        Route::get("/b", function () {
+            echo "xxxxxxxx";
+        });
     });
-    Route::get("/", function () {
+    Route::get("/c", function () {
         echo "xxxxxxxx";
-    });
+    }, ["ccc"]);
 });
-
 
 
